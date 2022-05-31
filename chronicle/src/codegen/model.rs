b88a9364367231cd69b27999e7942c11ceab43bf
Joseph Livesey <joseph@blockchaintp.com>
2022-05-31 10:10:17 -0400

    feat(/codegen): add `DomainFileInput` agents, entities, and activities fields
    Signed-off-by: Joseph Livesey <joseph@blockchaintp.com>

--- chronicle/src/codegen/model.rs
+++ chronicle/src/codegen/model.rs
@@ -300,6 +300,9 @@ pub struct AttributeFileInput {
 pub struct DomainFileInput {
     pub name: String,
     pub attributes: HashMap<String, AttributeFileInput>,
+    pub agents: HashMap<String, HashMap<String, AttributeFileInput>>,
+    pub entities: HashMap<String, HashMap<String, AttributeFileInput>>,
+    pub activities: HashMap<String, HashMap<String, AttributeFileInput>>,
 }
 
 impl ChronicleDomainDef {
