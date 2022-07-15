8cc347d3c6afaf250eeddaae67a209f9a9785d34
Joseph Livesey <joseph@blockchaintp.com>
2022-07-15 19:50:41 -0400

    feat(common): CHRON-39 enforce `LedgerAddress` write-to address validation
    Signed-off-by: Joseph Livesey <joseph@blockchaintp.com>

--- common/src/ledger.rs
+++ common/src/ledger.rs
@@ -227,6 +227,8 @@ impl LedgerWriter for InMemLedger {
         let mut model = ProvModel::default();
         let mut output = vec![];
 
+        let mut deps_addresses: Vec<LedgerAddress> = vec![];
+
         for tx in tx {
             let deps = tx.dependencies();
 
@@ -247,6 +249,7 @@ impl LedgerWriter for InMemLedger {
 
             output.append(&mut tx_output);
             model = updated_model;
+            deps_addresses = deps.into_iter().collect();
         }
 
         //Merge state output (last update wins) and sort by address, so push into a btree then iterate back to a vector
@@ -256,7 +259,11 @@ impl LedgerWriter for InMemLedger {
             .collect::<BTreeMap<_, _>>()
             .into_iter()
             .map(|x| x.1)
-            .collect::<Vec<_>>();
+            .map(|s| match s.address.is_specified(&deps_addresses) {
+                true => Ok(s),
+                _ => Err(ProcessorError::Address {}),
+            })
+            .collect::<Result<Vec<_>, ProcessorError>>()?;
 
         for output in output {
             let state = json::parse(from_utf8(&output.data).unwrap()).unwrap();
@@ -305,6 +312,10 @@ impl LedgerAddress {
             namespace: Some(ns.clone()),
             resource: resource.into(),
         }
+    }
+
+    fn is_specified(&self, dependencies: &[LedgerAddress]) -> bool {
+        dependencies.iter().any(|addr| self == addr)
     }
 }
 
@@ -555,4 +566,357 @@ impl ChronicleOperation {
 }
 
 #[cfg(test)]
-pub mod test {}
+pub mod test {
+    use serde_json::json;
+    use std::collections::BTreeMap;
+
+    use crate::{
+        attributes::{Attribute, Attributes},
+        ledger::{InMemLedger, LedgerWriter},
+        prov::{
+            operations::{
+                ActivityUses, ActsOnBehalfOf, ChronicleOperation, CreateActivity, CreateEntity,
+                CreateNamespace, DerivationType, EntityDerive, GenerateEntity, RegisterKey,
+                SetAttributes, StartActivity,
+            },
+            ActivityId, AgentId, DomaintypeId, EntityId, NamePart, NamespaceId,
+        },
+    };
+    use uuid::Uuid;
+    fn uuid() -> Uuid {
+        let bytes = [
+            0xa1, 0xa2, 0xa3, 0xa4, 0xb1, 0xb2, 0xc1, 0xc2, 0xd1, 0xd2, 0xd3, 0xd4, 0xd5, 0xd6,
+            0xd7, 0xd8,
+        ];
+        Uuid::from_slice(&bytes).unwrap()
+    }
+
+    // #[tokio::test]
+    // async fn test_entity_attach() -> Result<(), String> {
+    //     let namespace: NamespaceId = NamespaceId::from_name("testns", uuid());
+    //     let id = EntityId::from_name("test_entity");
+    //     let agent = AgentId::from_name("test_agent");
+    //     let op: ChronicleOperation = ChronicleOperation::EntityAttach(EntityAttach {
+    //         namespace,
+    //         identityid: None,
+    //         id,
+    //         locator: None,
+    //         agent,
+    //         signature: None,
+    //         signature_time: None,
+    //     });
+    //     let mut l = InMemLedger::new();
+    //     let tx = vec![op];
+    //     let id = l.submit(&tx).await;
+    //     if id.is_err() {
+    //         Err(format!("error: {id:?}"))
+    //     } else {
+    //         Ok(())
+    //     }
+    // }
+
+    #[tokio::test]
+    async fn test_create_namespace() -> Result<(), String> {
+        let name = "testns";
+        let id = NamespaceId::from_name(name, uuid());
+
+        let op = ChronicleOperation::CreateNamespace(CreateNamespace::new(id, name, uuid()));
+        let mut l = InMemLedger::new();
+        let tx = vec![op];
+        let id = l.submit(&tx).await;
+        if id.is_err() {
+            Err(format!("error: {id:?}"))
+        } else {
+            Ok(())
+        }
+    }
+
+    #[tokio::test]
+    async fn test_create_agent() -> Result<(), String> {
+        let uuid = uuid();
+        let namespace: NamespaceId = NamespaceId::from_name("testns", uuid);
+        let name: crate::prov::Name =
+            crate::prov::NamePart::name_part(&crate::prov::AgentId::from_name("test_agent"))
+                .clone();
+        let op: ChronicleOperation =
+            super::ChronicleOperation::CreateAgent(crate::prov::operations::CreateAgent {
+                namespace,
+                name,
+            });
+        let mut l = InMemLedger::new();
+        let tx = vec![op];
+        let id = l.submit(&tx).await;
+        if id.is_err() {
+            Err(format!("error: {id:?}"))
+        } else {
+            Ok(())
+        }
+    }
+
+    #[tokio::test]
+    async fn test_agent_acts_on_behalf_of() -> Result<(), String> {
+        let namespace: NamespaceId = NamespaceId::from_name("testns", uuid());
+        let id = AgentId::from_name("test_agent");
+        let delegate_id = AgentId::from_name("test_delegate");
+        let activity_id = Some(ActivityId::from_name("test_activity"));
+
+        let op: ChronicleOperation = ChronicleOperation::AgentActsOnBehalfOf(ActsOnBehalfOf {
+            namespace,
+            id,
+            delegate_id,
+            activity_id,
+        });
+        let mut l = InMemLedger::new();
+        let tx = vec![op];
+        let id = l.submit(&tx).await;
+        if id.is_err() {
+            Err(format!("error: {id:?}"))
+        } else {
+            Ok(())
+        }
+    }
+
+    #[tokio::test]
+    async fn test_register_key() -> Result<(), String> {
+        let namespace: NamespaceId = NamespaceId::from_name("testns", uuid());
+        let id = crate::prov::AgentId::from_name("test_agent");
+        let publickey =
+            "02197db854d8c6a488d4a0ef3ef1fcb0c06d66478fae9e87a237172cf6f6f7de23".to_string();
+
+        let op: ChronicleOperation = ChronicleOperation::RegisterKey(RegisterKey {
+            namespace,
+            id,
+            publickey,
+        });
+        let mut l = InMemLedger::new();
+        let tx = vec![op];
+        let id = l.submit(&tx).await;
+        if id.is_err() {
+            Err(format!("error: {id:?}"))
+        } else {
+            Ok(())
+        }
+    }
+
+    #[tokio::test]
+    async fn test_create_activity() -> Result<(), String> {
+        let namespace: NamespaceId = NamespaceId::from_name("testns", uuid());
+        let name = NamePart::name_part(&ActivityId::from_name("test_activity")).to_owned();
+
+        let op: ChronicleOperation =
+            ChronicleOperation::CreateActivity(CreateActivity { namespace, name });
+        let mut l = InMemLedger::new();
+        let tx = vec![op];
+        let id = l.submit(&tx).await;
+        if id.is_err() {
+            Err(format!("error: {id:?}"))
+        } else {
+            Ok(())
+        }
+    }
+
+    #[tokio::test]
+    async fn start_activity() -> Result<(), String> {
+        let namespace: NamespaceId = NamespaceId::from_name("testns", uuid());
+        let id = ActivityId::from_name("test_activity");
+        let agent = AgentId::from_name("test_agent");
+        let time = chrono::DateTime::<chrono::Utc>::from_utc(
+            chrono::NaiveDateTime::from_timestamp(61, 0),
+            chrono::Utc,
+        );
+        let op: ChronicleOperation = ChronicleOperation::StartActivity(StartActivity {
+            namespace,
+            id,
+            agent,
+            time,
+        });
+        let mut l = InMemLedger::new();
+        let tx = vec![op];
+        let id = l.submit(&tx).await;
+        if id.is_err() {
+            Err(format!("error: {id:?}"))
+        } else {
+            Ok(())
+        }
+    }
+
+    #[tokio::test]
+    async fn test_end_activity() -> Result<(), String> {
+        let namespace: NamespaceId = NamespaceId::from_name("testns", uuid());
+        let id = ActivityId::from_name("test_activity");
+        let agent = crate::prov::AgentId::from_name("test_agent");
+        let time = chrono::DateTime::<chrono::Utc>::from_utc(
+            chrono::NaiveDateTime::from_timestamp(61, 0),
+            chrono::Utc,
+        );
+        let op: ChronicleOperation =
+            super::ChronicleOperation::EndActivity(crate::prov::operations::EndActivity {
+                namespace,
+                id,
+                agent,
+                time,
+            });
+        let mut l = InMemLedger::new();
+        let tx = vec![op];
+        let id = l.submit(&tx).await;
+        if id.is_err() {
+            Err(format!("error: {id:?}"))
+        } else {
+            Ok(())
+        }
+    }
+
+    #[tokio::test]
+    async fn test_activity_uses() -> Result<(), String> {
+        let namespace: NamespaceId = NamespaceId::from_name("testns", uuid());
+        let id = EntityId::from_name("test_entity");
+        let activity = ActivityId::from_name("test_activity");
+        let op: ChronicleOperation = ChronicleOperation::ActivityUses(ActivityUses {
+            namespace,
+            id,
+            activity,
+        });
+        let mut l = InMemLedger::new();
+        let tx = vec![op];
+        let id = l.submit(&tx).await;
+        if id.is_err() {
+            Err(format!("error: {id:?}"))
+        } else {
+            Ok(())
+        }
+    }
+
+    #[tokio::test]
+    async fn test_create_entity() -> Result<(), String> {
+        let namespace: NamespaceId = NamespaceId::from_name("testns", uuid());
+        let id = NamePart::name_part(&EntityId::from_name("test_entity")).to_owned();
+        let op: ChronicleOperation = ChronicleOperation::CreateEntity(CreateEntity {
+            namespace,
+            name: id,
+        });
+        let mut l = InMemLedger::new();
+        let tx = vec![op];
+        let id = l.submit(&tx).await;
+        if id.is_err() {
+            Err(format!("error: {id:?}"))
+        } else {
+            Ok(())
+        }
+    }
+
+    #[tokio::test]
+    async fn test_generate_entity() -> Result<(), String> {
+        let namespace: NamespaceId = NamespaceId::from_name("testns", uuid());
+        let id = EntityId::from_name("test_entity");
+        let activity = ActivityId::from_name("test_activity");
+        let op: ChronicleOperation = ChronicleOperation::GenerateEntity(GenerateEntity {
+            namespace,
+            id,
+            activity,
+        });
+        let mut l = InMemLedger::new();
+        let tx = vec![op];
+        let id = l.submit(&tx).await;
+        if id.is_err() {
+            Err(format!("error: {id:?}"))
+        } else {
+            Ok(())
+        }
+    }
+
+    #[tokio::test]
+    async fn test_entity_derive() -> Result<(), String> {
+        let namespace: NamespaceId = NamespaceId::from_name("testns", uuid());
+        let id = EntityId::from_name("test_entity");
+        let used_id = EntityId::from_name("test_used_entity");
+        let activity_id = Some(ActivityId::from_name("test_activity"));
+        let typ = Some(DerivationType::Revision);
+        let op: ChronicleOperation = ChronicleOperation::EntityDerive(EntityDerive {
+            namespace,
+            id,
+            used_id,
+            activity_id,
+            typ,
+        });
+        let mut l = InMemLedger::new();
+        let tx = vec![op];
+        let id = l.submit(&tx).await;
+        if id.is_err() {
+            Err(format!("error: {id:?}"))
+        } else {
+            Ok(())
+        }
+    }
+
+    #[tokio::test]
+    async fn test_set_attributes_entity() -> Result<(), String> {
+        let namespace: NamespaceId = NamespaceId::from_name("testns", uuid());
+        let id = EntityId::from_name("test_entity");
+        let domain = DomaintypeId::from_name("test_domain");
+        let attributes = Attributes {
+            typ: Some(domain),
+            attributes: BTreeMap::new(),
+        };
+        let op: ChronicleOperation = ChronicleOperation::SetAttributes(SetAttributes::Entity {
+            namespace,
+            id,
+            attributes,
+        });
+        let mut l = InMemLedger::new();
+        let tx = vec![op];
+        let id = l.submit(&tx).await;
+        if id.is_err() {
+            Err(format!("error: {id:?}"))
+        } else {
+            Ok(())
+        }
+    }
+
+    #[tokio::test]
+    async fn test_set_attributes_entity_multiple_attributes() -> Result<(), String> {
+        let namespace: NamespaceId = NamespaceId::from_name("testns", uuid());
+        let id = EntityId::from_name("test_entity");
+        let domain = DomaintypeId::from_name("test_domain");
+        let attrs = {
+            let mut h: BTreeMap<String, Attribute> = BTreeMap::new();
+
+            let attr = Attribute {
+                typ: "Bool".to_string(),
+                value: json!("Bool"),
+            };
+            h.insert("bool_attribute".to_string(), attr);
+
+            let attr = Attribute {
+                typ: "String".to_string(),
+                value: json!("String"),
+            };
+            h.insert("string_attribute".to_string(), attr);
+
+            let attr = Attribute {
+                typ: "Int".to_string(),
+                value: json!("Int"),
+            };
+            h.insert("int_attribute".to_string(), attr);
+
+            h
+        };
+
+        let attributes = Attributes {
+            typ: Some(domain),
+            attributes: attrs,
+        };
+        let op: ChronicleOperation = ChronicleOperation::SetAttributes(SetAttributes::Entity {
+            namespace,
+            id,
+            attributes,
+        });
+        let mut l = InMemLedger::new();
+        let tx = vec![op];
+        let id = l.submit(&tx).await;
+        if id.is_err() {
+            Err(format!("error: {id:?}"))
+        } else {
+            Ok(())
+        }
+    }
+}
