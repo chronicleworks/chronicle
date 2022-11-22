dc1ea18b967a4c62377607cf26b322b4623e1e59
Mark T. B. Carroll <mark@blockchaintp.com>
2022-11-23 04:31:38 +0800

    feat(up.sql): use serial type for database keys
    CHRON-16
    
    Signed-off-by: Mark T. B. Carroll <mark@blockchaintp.com>

--- crates/api/migrations/2022-01-18-144301_ledger_sync/up.sql
+++ crates/api/migrations/2022-01-18-144301_ledger_sync/up.sql
@@ -1,5 +1,5 @@
 create table namespace (
-    id integer primary key not null,
+    id serial primary key,
     external_id text not null,
     uuid text not null,
     unique(external_id)
@@ -8,7 +8,7 @@ create table namespace (
 create unique index namespace_idx on namespace(external_id,uuid);
 
 create table ledgersync (
-    tx_id text primary key not null,
+    tx_id text primary key,
     offset text,
     sync_time timestamp
 );
@@ -16,12 +16,12 @@ create table ledgersync (
 create index ledger_index on ledgersync(sync_time,offset);
 
 create table agent (
-    id integer primary key not null,
-    external_id text key not null,
-    namespace_id integer not null,
+    id serial primary key,
+    external_id text not null,
+    namespace_id serial not null,
     domaintype text,
     current integer not null,
-    identity_id integer,
+    identity_id serial,
     foreign key(identity_id) references identity(id),
     foreign key(namespace_id) references namespace(id),
     unique(external_id,namespace_id)
@@ -30,8 +30,8 @@ create table agent (
 create index agent_external_id_idx on agent(external_id,namespace_id);
 
 create table identity (
-    id integer primary key not null,
-    namespace_id integer not null,
+    id serial primary key,
+    namespace_id serial not null,
     public_key text not null,
     foreign key(namespace_id) references namespace(id)
 );
@@ -39,9 +39,9 @@ create table identity (
 create index identity_public_key_idx on identity(public_key);
 
 create table activity (
-    id integer primary key not null,
+    id serial primary key,
     external_id text not null,
-    namespace_id integer not null,
+    namespace_id serial not null,
     domaintype text,
     started timestamp,
     ended timestamp,
@@ -50,22 +50,22 @@ create table activity (
 );
 
 create table entity (
-    id integer primary key not null,
+    id serial primary key,
     external_id text not null,
-    namespace_id integer not null,
+    namespace_id serial not null,
     domaintype text,
-    attachment_id integer,
+    attachment_id serial,
     foreign key(attachment_id) references attachment(id),
     foreign key(namespace_id) references namespace(id),
     unique(external_id,namespace_id)
 );
 
 create table attachment (
-    id integer primary key not null,
-    namespace_id integer not null,
+    id serial primary key,
+    namespace_id serial not null,
     signature_time timestamp not null,
     signature text not null,
-    signer_id integer not null,
+    signer_id serial not null,
     locator text,
     foreign key(namespace_id) references namespace(id),
     foreign key(signer_id) references identity(id)
@@ -74,9 +74,9 @@ create table attachment (
 create index attachment_signature_idx on attachment(signature);
 
 create table delegation (
-    delegate_id integer not null,
-    responsible_id integer not null,
-    activity_id integer,
+    delegate_id serial not null,
+    responsible_id serial not null,
+    activity_id serial,
     role text,
     foreign key(delegate_id) references agent(id),
     foreign key(responsible_id) references agent(id),
@@ -86,9 +86,9 @@ create table delegation (
 );
 
 create table derivation (
-    activity_id integer,
-    generated_entity_id integer not null,
-    used_entity_id integer not null,
+    activity_id serial,
+    generated_entity_id serial not null,
+    used_entity_id serial not null,
     typ integer,
     foreign key(activity_id) references activity(id),
     foreign key(generated_entity_id) references entity(id),
@@ -98,8 +98,8 @@ create table derivation (
 );
 
 create table generation (
-    activity_id integer not null,
-    generated_entity_id integer not null,
+    activity_id serial not null,
+    generated_entity_id serial not null,
     typ text,
     foreign key(activity_id) references activity(id),
     foreign key(generated_entity_id) references entity(id),
@@ -107,8 +107,8 @@ create table generation (
 );
 
 create table association (
-    agent_id integer not null,
-    activity_id integer not null,
+    agent_id serial not null,
+    activity_id serial not null,
     role text,
     foreign key(agent_id) references agent(id),
     foreign key(activity_id) references activity(id),
@@ -117,58 +117,57 @@ create table association (
 );
 
 create table usage (
-    activity_id integer not null,
-    entity_id integer not null,
+    activity_id serial not null,
+    entity_id serial not null,
     foreign key(entity_id) references entity(id),
     foreign key(activity_id) references activity(id),
     primary key(activity_id,entity_id)
 );
 
 create table wasinformedby (
-    activity_id integer not null,
-    informing_activity_id integer not null,
+    activity_id serial not null,
+    informing_activity_id serial not null,
     foreign key(activity_id) references activity(id),
     foreign key(informing_activity_id) references activity(id),
     primary key(activity_id,informing_activity_id)
 );
 
 create table hadidentity (
-    agent_id integer not null,
-    identity_id integer not null,
+    agent_id serial not null,
+    identity_id serial not null,
     foreign key(agent_id) references agent(id),
     foreign key(identity_id) references identity(id),
     primary key(agent_id,identity_id)
 );
 
 create table hadattachment (
-    entity_id integer not null,
-    attachment_id integer not null,
-    foreign key(entity_id ) references entity(id),
+    entity_id serial not null,
+    attachment_id serial not null,
+    foreign key(entity_id) references entity(id),
     foreign key(attachment_id) references attachment(id),
     primary key(entity_id,attachment_id)
 );
 
-
 create table entity_attribute (
-    entity_id integer not null,
+    entity_id serial not null,
     typename text not null,
     value text not null,
-    foreign key(entity_id ) references entity(id),
+    foreign key(entity_id) references entity(id),
     primary key(entity_id,typename)
 );
 
 create table agent_attribute (
-    agent_id integer not null,
+    agent_id serial not null,
     typename text not null,
     value text not null,
-    foreign key(agent_id ) references agent(id),
+    foreign key(agent_id) references agent(id),
     primary key(agent_id,typename)
 );
 
 create table activity_attribute (
-    activity_id integer not null,
+    activity_id serial not null,
     typename text not null,
     value text not null,
-    foreign key(activity_id ) references activity(id),
+    foreign key(activity_id) references activity(id),
     primary key(activity_id,typename)
 );
