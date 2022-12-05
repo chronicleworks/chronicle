ae45f55c6b078fef9ca4bb43d54fee64ab97dad9
Mark T. B. Carroll <mark.carroll@btp.works>
2022-12-05 11:53:20 +0000

    fix(database.rs): panic and report the database directory path
    not for merging, just for discovering the path in ci
    
    Signed-off-by: Mark T. B. Carroll <mark.carroll@btp.works>

--- crates/common/src/database.rs
+++ crates/common/src/database.rs
@@ -2,14 +2,12 @@ use diesel::{r2d2::ConnectionManager, PgConnection};
 use pg_embed::{
     self,
     pg_enums::PgAuthMethod,
-    pg_fetch::PgFetchSettings,
     pg_types::PgResult,
     postgres::{self, PgEmbed},
 };
 use r2d2::Pool;
 use std::time::Duration;
 use temp_dir::TempDir;
-use uuid::Uuid;
 
 pub struct Database {
     _embedded: PgEmbed,
@@ -29,20 +27,5 @@ pub async fn get_embedded_db_connection(
         timeout: Some(Duration::from_secs(50)),
         migration_dir: None,
     };
-    let mut database = PgEmbed::new(settings, PgFetchSettings::default()).await?;
-    database.setup().await?;
-    database.start_db().await?;
-    let db_name = format!("chronicle-{}", Uuid::new_v4());
-    database.create_database(db_name.as_str()).await?;
-    let db_uri = database.full_db_uri(&db_name);
-    let pool = Pool::builder()
-        .build(ConnectionManager::<PgConnection>::new(db_uri))
-        .unwrap();
-    Ok((
-        Database {
-            _embedded: database,
-            _location: temp_dir,
-        },
-        pool,
-    ))
+    panic!("database directory path is {:?}", settings.database_dir);
 }
