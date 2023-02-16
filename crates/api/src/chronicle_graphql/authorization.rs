b4ab9eced1381985d941646deaac0950df7cb251
Mark T. B. Carroll <mark.carroll@btp.works>
2023-02-16 11:22:05 +0000

    feat(authorization.rs): cache responses from oidc /userinfo endpoint
    CHRON-237
    
    Signed-off-by: Mark T. B. Carroll <mark.carroll@btp.works>

--- crates/api/src/chronicle_graphql/authorization.rs
+++ crates/api/src/chronicle_graphql/authorization.rs
@@ -1,9 +1,11 @@
 use base64::Engine;
+use cached::{Cached, TimedCache};
 use jwtk::jwk::RemoteJwksVerifier;
 use reqwest::StatusCode;
 use serde_json::{Map, Value};
-use std::time::Duration;
+use std::{sync::Arc, time::Duration};
 use thiserror::Error;
+use tokio::sync::Mutex;
 use tracing::instrument;
 use url::Url;
 
@@ -39,15 +41,23 @@ pub struct JwtChecker {
     client: reqwest::Client,
     verifier: RemoteJwksVerifier,
     userinfo_uri: Option<String>,
+    userinfo_cache: Arc<Mutex<TimedCache<String, Map<String, Value>>>>,
 }
 
 impl JwtChecker {
     #[instrument(level = "debug")]
-    pub fn new(jwks_uri: &Url, userinfo_uri: Option<&Url>) -> Self {
+    pub fn new(jwks_uri: &Url, userinfo_uri: Option<&Url>, cache_expiry_seconds: u32) -> Self {
         Self {
             client: reqwest::Client::new(),
-            verifier: RemoteJwksVerifier::new(jwks_uri.to_string(), None, Duration::from_secs(100)),
+            verifier: RemoteJwksVerifier::new(
+                jwks_uri.to_string(),
+                None,
+                Duration::from_secs(cache_expiry_seconds.into()),
+            ),
             userinfo_uri: userinfo_uri.map(|s| s.to_string()),
+            userinfo_cache: Arc::new(Mutex::new(TimedCache::with_lifespan(
+                cache_expiry_seconds.into(),
+            ))),
         }
     }
 
@@ -87,35 +97,49 @@ impl JwtChecker {
             Err(err) => error = Some(err), // could tolerate error from what may be opaque token
         };
         if let Some(userinfo_uri) = &self.userinfo_uri {
-            let request = self
-                .client
-                .post(userinfo_uri)
-                .header("Authorization", format!("Bearer {token}"));
-            let response = request.send().await?;
-            if response.status() == 200 {
-                let response_text = &response.text().await?;
-                if let Ok(claims_from_userinfo) = self.attempt_jwt(response_text).await {
-                    error = None;
-                    claims.extend(claims_from_userinfo)
-                } else if let Ok(Value::Object(claims_from_userinfo)) =
-                    serde_json::from_str(response_text)
-                {
-                    error = None;
-                    claims.extend(claims_from_userinfo)
+            let mut cache = self.userinfo_cache.lock().await;
+            if let Some(claims_from_userinfo) = cache.cache_get(&token.to_string()) {
+                tracing::trace!("userinfo cache hit");
+                error = None;
+                claims.extend(claims_from_userinfo.clone());
+            } else {
+                tracing::trace!("userinfo cache miss");
+                drop(cache);
+                let request = self
+                    .client
+                    .post(userinfo_uri)
+                    .header("Authorization", format!("Bearer {token}"));
+                let response = request.send().await?;
+                cache = self.userinfo_cache.lock().await;
+                if response.status() == 200 {
+                    let response_text = &response.text().await?;
+                    if let Ok(claims_from_userinfo) = self.attempt_jwt(response_text).await {
+                        error = None;
+                        claims.extend(claims_from_userinfo.clone());
+                        cache.cache_set(token.to_string(), claims_from_userinfo);
+                    } else if let Ok(Value::Object(claims_from_userinfo)) =
+                        serde_json::from_str(response_text)
+                    {
+                        error = None;
+                        claims.extend(claims_from_userinfo.clone());
+                        cache.cache_set(token.to_string(), claims_from_userinfo);
+                    } else {
+                        error = Some(Error::Format {
+                            message: format!(
+                                "UserInfo response has unexpected format: {response_text}"
+                            ),
+                        })
+                    }
+                } else if error.is_some() {
+                    tracing::trace!("first error before UserInfo was {error:?}");
+                    error = Some(Error::UserInfoResponse {
+                        server: userinfo_uri.clone(),
+                        status: response.status(),
+                    });
                 } else {
-                    error = Some(Error::Format {
-                        message: format!(
-                            "UserInfo response has unexpected format: {response_text}"
-                        ),
-                    })
+                    cache.cache_set(token.to_string(), Map::new());
                 }
-            } else if error.is_some() {
-                tracing::trace!("first error before UserInfo was {error:?}");
-                error = Some(Error::UserInfoResponse {
-                    server: userinfo_uri.clone(),
-                    status: response.status(),
-                })
-            };
+            }
         }
         if let Some(error) = error {
             Err(error)
