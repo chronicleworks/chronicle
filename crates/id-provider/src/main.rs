b92553a3d9ffbe04f3ab9f8a8625bd1bafe6a5a5
Joseph Livesey <joseph.livesey@btp.works>
2023-06-16 12:32:29 -0400

    fix(id-provider): fix id-provider to login to get token
    Signed-off-by: Joseph Livesey <joseph.livesey@btp.works>

--- crates/id-provider/src/main.rs
+++ crates/id-provider/src/main.rs
@@ -2,66 +2,43 @@ use oauth2::basic::BasicClient;
 use oauth2::reqwest::http_client;
 use oauth2::{
     AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken, PkceCodeChallenge, RedirectUrl,
-    Scope, TokenResponse, TokenUrl,
+    ResponseType, Scope, TokenResponse, TokenUrl,
 };
-use std::process::Command;
-use url::Url;
-
 fn main() -> Result<(), anyhow::Error> {
-    // construct OAuth query: authorization code flow with PKCE
-
     let oauth_client = BasicClient::new(
-        ClientId::new("client-id".to_string()),
-        Some(ClientSecret::new("client-secret".to_string())),
-        AuthUrl::new("http://localhost:8090/authorize".to_string())?,
-        Some(TokenUrl::new("http://localhost:8090/token".to_string())?),
+        ClientId::new("hXaf3vs5oeL0y2AJPBh9BEeGkD3e3u3e".to_string()),
+        Some(ClientSecret::new(
+            "PakBX-b-sV4dplt5tjbIr56FoXkBFWQEvm2ZVgzej2rySRWcqr9qvVScF3btmKEc".to_string(),
+        )),
+        AuthUrl::new("https://dev-cha-9aet.us.auth0.com/authorize".to_string())?,
+        Some(TokenUrl::new(
+            "https://dev-cha-9aet.us.auth0.com/oauth/token".to_string(),
+        )?),
     )
-    .set_redirect_uri(RedirectUrl::new("http://example.com/callback".to_string())?);
+    .set_redirect_uri(RedirectUrl::new("https://dev-cha-9aet.us.auth0.com/authorize".to_string())?);
 
     let (pkce_challenge, pkce_verifier) = PkceCodeChallenge::new_random_sha256();
 
-    let (auth_url, csrf_token) = oauth_client
+    let (auth_url, _csrf_token) = oauth_client
         .authorize_url(CsrfToken::new_random)
         .add_scope(Scope::new("openid".to_string()))
         .add_scope(Scope::new("profile".to_string()))
         .add_scope(Scope::new("email".to_string()))
         .set_pkce_challenge(pkce_challenge)
+        .set_response_type(&ResponseType::new("code".to_string())) // Add this line to set the response type
         .url();
 
-    // use curl to handle HTTP basic authentication
+    // Open the authorization URL in the default browser
+    open::that(auth_url.to_string())?;
 
-    let args = vec![
-        "-w".to_string(),
-        "%{redirect_url}\n".to_string(),
-        "-u".to_string(),
-        "rmalina1:test-password".to_string(),
-        auth_url.to_string(),
-    ];
-
-    let curl_output = Command::new("curl").args(args).output()?;
-
-    // parse URL from redirect to callback with authorization code
-
-    let url = Url::parse(std::str::from_utf8(&curl_output.stdout)?.trim())?;
-
-    let mut query_state = None;
-    let mut query_code = None;
-
-    for (key, value) in url.query_pairs() {
-        match key.to_string().as_str() {
-            "state" => query_state = Some(value),
-            "code" => query_code = Some(value),
-            _ => {}
-        }
-    }
-
-    assert_eq!(*csrf_token.secret(), query_state.unwrap().to_string());
+    // Retrieve the authorization code manually after authentication
+    println!("Please enter the authorization code:");
+    let mut auth_code = String::new();
+    std::io::stdin().read_line(&mut auth_code)?;
 
     // exchange authorization code for access token
-
-    let auth_code = query_code.unwrap();
     let token_response = oauth_client
-        .exchange_code(AuthorizationCode::new(auth_code.to_string()))
+        .exchange_code(AuthorizationCode::new(auth_code.trim().to_string()))
         .set_pkce_verifier(pkce_verifier)
         .request(http_client)?;
 
